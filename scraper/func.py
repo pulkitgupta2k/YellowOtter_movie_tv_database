974281f9873af2433d282b3a22ee6d51c61a2dea
Pulkit Gupta <pulkitgupta2k@gmail.com>
2020-07-21 18:34:23 +0530

    single movie data

--- scraper/func.py
+++ scraper/func.py
@@ -20,8 +20,27 @@ def get_imdb_info_1():
             script_data = get_page_data_1(page)
 
 def get_page_data_1(soup):
-    data = soup.find("script", {"type": "application/ld+json"})
-    print(data.text)
+    data = json.loads(soup.find("script", {"type": "application/ld+json"}).text)
+
+
+    t_id = data['url'][7:-1]
+    name = data['name']
+    desc = data['description']
+    genre = data['genre']   #list
+    IMDB_rating = data['aggregateRating']['ratingValue']
+    Rotten_rating = None
+    age_rating = data['contentRating']
+    cover_image = data['image']
+    air_date = data['datePublished']
+    trailer_link = "#"
+
+    cast = []
+    tv_season = []
+    epidode = []
+
+    ret_data = [t_id, name, desc, genre, IMDB_rating, Rotten_rating, age_rating, cover_image, air_date, trailer_link, cast, tv_season, epidode]
+    pprint(ret_data)
+    return ret_data
 
 def driver():
     # clean_tsv()
