ce938ad5b463ef188c01f386e65d297bb663acab
Pulkit Gupta <pulkitgupta2k@gmail.com>
2020-07-21 18:46:05 +0530

    handled errors

--- scraper/func.py
+++ scraper/func.py
@@ -14,24 +14,41 @@ def get_imdb_info_1():
     links = []
     for row in data:
         links.append(f"https://www.imdb.com/title/{row[0]}/")
-    for i in range(0, len(links), 1000):
-        pages = getSoup_list(links[i : i+1000])
+    for i in range(0, len(links), 300):
+        pages = getSoup_list(links[i : i+300])
         for page in pages:
             script_data = get_page_data_1(page)
 
 def get_page_data_1(soup):
     data = json.loads(soup.find("script", {"type": "application/ld+json"}).text)
 
-
     t_id = data['url'][7:-1]
     name = data['name']
-    desc = data['description']
-    genre = data['genre']   #list
-    IMDB_rating = data['aggregateRating']['ratingValue']
+    try:
+        desc = soup.find("div", {"class": "plot_summary"}).text
+    except:
+        desc = ""
+    try:
+        genre = data['genre']   #list
+    except:
+        genre = ""
+    try:
+        IMDB_rating = data['aggregateRating']['ratingValue']
+    except:
+        IMDB_rating = ""
     Rotten_rating = None
-    age_rating = data['contentRating']
-    cover_image = data['image']
-    air_date = data['datePublished']
+    try: 
+        age_rating = data['contentRating']
+    except:
+        age_rating = ""
+    try:
+        cover_image = data['image']
+    except:
+        cover_image = ""
+    try:
+        air_date = data['datePublished']
+    except:
+        air_date = ""
     trailer_link = "#"
 
     cast = []
@@ -44,5 +61,5 @@ def get_page_data_1(soup):
 
 def driver():
     # clean_tsv()
-    # get_imdb_info_1()
-    get_page_data_1(getSoup("https://www.imdb.com/title/tt4574334/"))
+    get_imdb_info_1()
+    # get_page_data_1(getSoup("https://www.imdb.com/title/tt0000002/"))
