b9dcd580bfb6025527baf178ca121f02020ab056
Kush Daga <kushdaga@hotmail.com>
2020-07-23 20:38:11 +0530

    Added nav bar

---
+++ website/client/src/components/Nav/index.js
@@ -0,0 +1,23 @@
+/** @format */
+
+import React from "react"
+import { NavContainer, NavSubContainer } from "./styles"
+
+function Nav() {
+	return (
+		<NavContainer>
+			<NavSubContainer>
+				<ul className='nav-links'>
+					<li>Home</li>
+					<li>New</li>
+					<li>Popular</li>
+					<li>Movies</li>
+					<li>Tv Series</li>
+				</ul>
+				<input placeholder='Search for movies or tv shows' />
+			</NavSubContainer>
+		</NavContainer>
+	)
+}
+
+export default Nav
--- website/client/src/routes/index.js
+++ website/client/src/routes/index.js
@@ -4,10 +4,11 @@ import React from "react"
 import { Switch } from "react-router-dom"
 import Route from "./Route"
 import Home from "../pages/Home"
-
+import Nav from "../components/Nav"
 const Routes = () => {
 	return (
 		<>
+			<Nav />
 			<Switch>
 				<Route exact path='/' component={Home} />
 			</Switch>
