6f107089941be796720adea5634553cddee3e560
Pulkit Gupta <pulkitgupta2k@gmail.com>
2020-08-08 16:42:25 +0530

    ready for test

--- scraper/func.py
+++ scraper/func.py
@@ -3,16 +3,32 @@ from pprint import pprint
 
 RANGE_OF_SOUP = 600
 
+
+#tsv to json
 def clean_tsv():
     data = ret_tsv("data/data.tsv")
     ids = []
     for row in data:
         if row[1] != 'tvEpisode':
             ids.append([row[0], row[1]])
-    write_json(ids, "smol_data.json")
+    write_json(ids, "data/smol_data.json")
 
+# adding titleType to the json
+def add_ep(): 
+    ready_1 = ret_json("data/ready_1.json")
+    rem_1 = ret_json("data/smol_data.json")
+
+    for tid in rem_1:
+        try:
+            if len(ready_1[tid[0]]) < 14:
+                ready_1[tid[0]].append(tid[1])
+        except:
+            pass
+    write_json(ready_1, "data/ready_1.json")
+
+# First scrape getting general data
 def get_imdb_info_1():
-    data = ret_json("data/rem_0.json")
+    data = ret_json("data/smol_data.json")
     links = []
     script_data = {}
     for row in data:
@@ -27,9 +43,10 @@ def get_imdb_info_1():
                 pass
         if i % (10*RANGE_OF_SOUP) == 0:
             print(i)
-            write_json(script_data, "data/remaining_data.json")
-    write_json(script_data, "data/remaining_data.json")
+            write_json(script_data, "data/ready_1.json")
+    write_json(script_data, "data/ready_1.json")
 
+# First scrape helper
 def get_page_data_1(soup):
     data = soup.find("script", {"type": "application/ld+json"})
     data = json.loads(data.contents[0])
@@ -73,8 +90,10 @@ def get_page_data_1(soup):
     # pprint(ret_data)
     return ret_data
 
+
+# Second Scrape getting cast details
 def get_page_info_2():
-    data = ret_json("data/final_data_0.json")
+    data = ret_json("data/ready_1.json")
     links = []
     for t_id in data.keys():
         links.append(f"https://www.imdb.com/title/{t_id}/fullcredits")
@@ -88,10 +107,10 @@ def get_page_info_2():
                 pass
         if i % (10*RANGE_OF_SOUP) == 0:
             print(i)
-            write_json(data, "data/final_data_new.json")
-    write_json(data, "data/final_data_new.json")
+            write_json(data, "data/ready_1.json")
+    write_json(data, "data/ready_1.json")
 
-
+# Second Scrape Helper
 def get_page_data_2(soup):
     fullcredits_content = soup.find("div", {"id": "fullcredits_content"})
     headings_soup = fullcredits_content.findAll("h4")
@@ -134,20 +153,27 @@ def get_page_data_2(soup):
                     pass
     return [ t_id, cast]
 
+
+# Third Scrape getting seasons
 def get_page_info_3():
-    data = ret_json("data/final_data.json")
+    data = ret_json("data/ready_1.json")
     links = []
     for t_id_k, t_id_v in data.items():
         if t_id_v[13] == "tvSeries" or t_id_v[13] == "tvMiniSeries":
             links.append(f"https://www.imdb.com/title/{t_id_k}/episodes")
     for i in range(0, len(links),  RANGE_OF_SOUP):
         pages = getSoup_list(links[i : i+RANGE_OF_SOUP])
-        script_data = {}
         for page in pages:
-            t_id_data = get_page_data_2(page)
-            script_data[ t_id_data[0] ] [11]= t_id_data[1]
-        write_json("data/final_data.json", script_data)
+            try:
+                t_id_data = get_page_data_3(page)
+                data[ t_id_data[0] ] [11]= t_id_data[1]
+            except:
+                pass
+        if i % (10*RANGE_OF_SOUP) == 0:
+            write_json(data, "data/ready_1.json")
+        write_json(data, "data/ready_1.json")
 
+#third scrape helper
 def get_page_data_3(soup):
     t_id = soup.find("link", {"rel": "canonical"})["href"].split("/")[-2]
     season_soup = soup.find("select", {"id": "bySeason"}).findAll("option")
@@ -168,9 +194,9 @@ def get_page_data_3(soup):
         ret.append([season, years[i]])
     return [t_id, ret]
 
-
+# fourth scrape getting episodes
 def get_page_info_4():
-    data = ret_json("data/final_data.json")
+    data = ret_json("data/ready_1.json")
     links = []
     for t_id_k, t_id_v in data.items():
         if t_id_v[11]:
@@ -187,10 +213,10 @@ def get_page_info_4():
                 pass
         if i % (10*RANGE_OF_SOUP) == 0:
             print(i)
-            write_json(data, "data/final_data_2.json")
-    write_json(data, "data/final_data_2.json")
+            write_json(data, "data/ready_1.json")
+    write_json(data, "data/ready_1.json")
 
-
+# fourth scrape helper
 def get_page_data_4(soup):
     t_id = soup.find("link", {"rel": "canonical"})["href"].split("/")[-2]
     eps = soup.find("div", {"class": "list detail eplist"})
@@ -198,7 +224,6 @@ def get_page_data_4(soup):
     ret = []
 
     s = soup.find("select", {"id": "bySeason"}).find("option", {"selected": "selected"}).text.strip()
-    print(s)
     series_id = soup.find("link", {"rel": "canonical"})["href"].split("/")[-2]
     for ep in eps:
         ep_id = ep.find("a", {"itemprop": "name"})['href'].split("/")[-2]
@@ -214,13 +239,14 @@ def get_page_data_4(soup):
         except:
             ep_rating = ""
         ret.append([ep_id, series_id, ep_no, ep_name, air_date, ep_rating, ep_image])
-    pprint([t_id, ret])
     return [t_id, ret]
 
+
+# main driver
 def driver():
-    # clean_tsv()
-    # get_imdb_info_1()
-    # get_page_data_1(getSoup("https://www.imdb.com/title/tt0000002/"))
-    # get_page_data_3(getSoup("https://www.imdb.com/title/tt0000001/fullcredits"))
-    # print(get_page_data_3(getSoup("https://www.imdb.com/title/tt2898910/episodes")))
-    get_page_data_4(getSoup("https://www.imdb.com/title/tt4574334/episodes?season=4"))
+    clean_tsv()
+    get_imdb_info_1()
+    add_ep()
+    get_page_info_2()
+    get_page_info_3()
+    get_page_info_4()
