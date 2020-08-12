b1f4d44ae8f0717f7c7a037fd9ed691458afcebd
Jaskeerart Singh <jaskee789@gmail.com>
2020-08-12 19:47:25 +0530

    additional setup

--- website/server/app/database/index.js
+++ website/server/app/database/index.js
@@ -5,7 +5,16 @@ const URL = process.env.DATABASE_URL
   : "mongodb://localhost:27017/cool_db";
 
 const connectToDB = () => {
-  return mongoose.connect(URL, config);
+  return mongoose
+    .connect(URL, config)
+    .then(() => {
+      console.log("Connected To DB");
+      return Promise.resolve();
+    })
+    .catch(() => {
+      console.error(`Failed to Connect To ${URL}`);
+      Promise.reject();
+    });
 };
 module.exports = {
   connectToDB,
